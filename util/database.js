43ee302b3ea2455fc23945a71151daf8ef7bc039
Anchal98 <anchal1998gupta@gmail.com>
2019-03-23 15:51:35 +0530

    Adding image uploader

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize  = require('sequelize');
 
-const sequelize  = new Sequelize('node-complete','root','password',{
+const sequelize  = new Sequelize('node_complete','root','test',{
     dialect:'mysql',
     host :'localhost'
 
