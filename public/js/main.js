e1fbcecacd3568af775baae47071f53647484443
Anchal98 <anchal1998gupta@gmail.com>
2019-03-25 23:35:51 +0530

    Adding image carousel

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const backdrop = document.querySelector('.backdrop');
 const sideDrawer = document.querySelector('.mobile-nav');
 const menuToggle = document.querySelector('#side-menu-toggle');
 
+var slide_index = 1;
 function backdropClickHandler() {
   backdrop.style.display = 'none';
   sideDrawer.classList.remove('open');
@@ -11,6 +12,27 @@ function menuToggleClickHandler() {
   backdrop.style.display = 'block';
   sideDrawer.classList.add('open');
 }
+function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+
+function displaySlides(n){
+
+  var i;  
+  var slides = document.getElementsByClassName("showSlide");  
+  if (n > slides.length) { slide_index = 1 }  
+  if (n < 1) { slide_index = slides.length }  
+  for (i = 0; i < slides.length; i++) {  
+        slides[i].style.display = "none";  
+       }  
+    slides[slide_index - 1].style.display = "block";  
+
+}
+
+displaySlides(slide_index);
 
 backdrop.addEventListener('click', backdropClickHandler);
 menuToggle.addEventListener('click', menuToggleClickHandler);
